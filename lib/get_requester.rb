d60e0d3b45075373aa58dfb8c088bef8285e1a6a
Yuyi <yuyi365@gmail.com>
2021-11-30 16:07:57 -0500

    commit tree c3441bb623493e409423e04db352a9a2d26933f1

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,20 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    # we use the JSON library to parse the API response into nicely formatted JSON
+    JSON.parse(get_response_body)
+  end
+end
